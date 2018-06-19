ef95d7e872598d06bf1889a453f87faa53d73adf
Rojbi Seif <rojbiseif@gmail.com>
2018-06-19 16:26:25 +0200

    creation des deux vues

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,33 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+		
+		$client=$this->container->get('app.blackmirror');
+		$response=$client->getLists('/blackmirror'); 
+	
+
+		
+		
+		    return $this->render('@App/Root/AdnIndex.html.twig', array(
+            'films' => $response,
+        ));
+    }
+	
+	 /**
+     * @Route("/detail/{uniqId}", name="detail_serie")
+     */
+    public function DetailAction($uniqId)
+    {
+		
+		
+		  $client=$this->container->get('csa_guzzle.client.tvmaze');
+		  $response=$client->get('/episodes/'.$uniqId); 
+		  
+		  $data=  json_decode($response->getBody()->getContents());
+		  
+		
+         return $this->render('@App/Root/detail.html.twig', array(
+            'data' => $data,
+        )); 
     }
 }
